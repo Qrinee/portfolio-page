10265ec76626e6b2ac9c99aa16baa0e3507e37bd
Qrinee <kajman5021@gmail.com>
2024-11-08 11:02:21 +0100

    test

--- src/components/ParafiaWierzawice.jsx
+++ src/components/ParafiaWierzawice.jsx
@@ -1,21 +1,36 @@
 import React, { useState } from 'react'
 import Page from './Page'
 import video from '../assets/videos/parafia.mp4'
+
 export default function ParafiaWierzawice() {
   const [load, setLoad] = useState(false)
+
+  const handleVideoLoad = () => {
+    setLoad(true)
+  }
+
   return (
     <>
-        <h1 className='thin'>Parafia Wierzawice</h1>
-        <p>Parafia Matki Bożej Wspomożenia Wiernych w Wierzawicach – Roman Catholic parish located in the Archdiocese of Przemyśl, within the Leżajsk I Deanery.</p>
-        <div className='load' style={{display: load ? 'block' : 'none'}}>
-          <div class="loader"></div>
-        </div>
-        <video src={video} onLoad={() => setLoad(true)} autoPlay muted loop  className='video' style={{display: load ? 'none' : 'block'}}></video>
-        <a href='https://wierzawice.przemyska.pl/' target='_blank'>
-            <button className='btn'>Watch Demo</button>
-        </a>
-
+      <h1 className='thin'>Parafia Wierzawice</h1>
+      <p>Parafia Matki Bożej Wspomożenia Wiernych w Wierzawicach – Roman Catholic parish located in the Archdiocese of Przemyśl, within the Leżajsk I Deanery.</p>
+      
+      <div className='load' style={{ display: load ? 'none' : 'block' }}>
+        <div className="loader"></div>
+      </div>
+      
+      <video 
+        src={video} 
+        onLoadedData={handleVideoLoad} 
+        autoPlay 
+        muted 
+        loop 
+        className='video' 
+        style={{ display: load ? 'block' : 'none' }}
+      ></video>
+      
+      <a href='https://wierzawice.przemyska.pl/' target='_blank' rel='noopener noreferrer'>
+        <button className='btn'>Watch Demo</button>
+      </a>
     </>
-
   )
 }
